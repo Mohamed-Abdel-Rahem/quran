0303a48a6e9bb374634af21061362f34f1f8eb3c
Mohamed-Abdel-Rahem <mohammedabdelrahem202123067@gmail.com>
2024-07-25 08:32:46 +0300

    add ellipse folder to assets

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,11 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:intl/intl.dart';
 import 'package:quran/generated/l10n.dart';
 import 'package:quran/screens/quran_page.dart';
-import 'package:device_preview/device_preview.dart';
 
 void main() {
-  runApp(DevicePreview(
-    enabled: true,
-    tools: [...DevicePreview.defaultTools],
-    builder: (context) => QuranApp(),
-  ));
+  runApp(
+    const QuranApp(),
+  );
 }
 
 class QuranApp extends StatelessWidget {
