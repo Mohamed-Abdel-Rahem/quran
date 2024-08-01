47673e6c46a32abb192f10d31be125b923fbafc7
Mohamed-Abdel-Rahem <mohammedabdelrahem202123067@gmail.com>
2024-08-01 13:24:15 +0300

    add images of moton

--- lib/screens/mton.dart
+++ lib/screens/mton.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:quran/widgets/backgroun_build.dart';
 
 class MtonPage extends StatelessWidget {
   const MtonPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Stack(
+        children: [
+          const BackgrounBuild(),
+          Positioned(
+            top: -screenSize.width * 0.25,
+            right: -screenSize.width * 0.25,
+            child: Image.asset(
+              'assets/ellipse/Ellipse 10.png',
+              width: screenSize.width * 1.1,
+              height: screenSize.width * 1.1,
+            ),
+          ),
+          //content
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [],
+          )
+          // content
+        ],
+      ),
+    );
   }
 }
